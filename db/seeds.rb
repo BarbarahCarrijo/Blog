e3c895b8bb46677da84e648ff5ac6c490bc58db4
Barbara Carrijo <barbarahcarrijo@gmail.com>
2019-11-24 17:01:41 -0300

    Adicionado artigo e comentário inicial via seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+article = Article.create( name: 'Star Wars' )
+
+Comment.create(name: 'Gostaria de assistir, porem ainda não', article: article)
